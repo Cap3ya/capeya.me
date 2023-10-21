548d9713f974b9723a4430a1d63269ae7bebbd12
Capeya <capeya92@gmail.com>
2023-10-21 02:10:27 +0200

    Move projects to its own file

---
+++ assets/projects.js
@@ -0,0 +1,62 @@
+export const projects = [
+    {
+        title: "Rock Paper Scissors",
+        image: {
+            src: "./images/rockPaperScissors.png", 
+            alt: "Screenshot", 
+            height: "205",
+            width: "364",
+        },
+        skills: ["HTML", "CSS", "JavaScript"],
+        text: "Play against a well behaved bot and try, in five rounds, to win the game!",
+        button: {
+            href: "https://Cap3ya.github.io/RockPaperScissors/rockPaperScissors.html",
+            text: "Beat him"
+        },
+    },
+    {
+        title: "Calculator",
+        image: {
+            src: "./images/calculator.png", 
+            alt: "Screenshot", 
+            height: "205",
+            width: "364",
+        },
+        skills: ["HTML", "CSS", "JavaScript"],
+        text: "A calculator that allows to perform the four basic arithmetic operations.",
+        button: {
+            href: "https://Cap3ya.github.io/Calculator/calculator.html",
+            text: "Sum up"
+        },
+    },
+    {
+        title: "Sign up Page",
+        image: {
+            src: "./images/signupPage.png", 
+            alt: "Screenshot", 
+            height: "205",
+            width: "364",
+        },
+        skills: ["HTML", "CSS", "JavaScript"],
+        text: "A sign up page that uses forms, inputs and sanitization to signup.",
+        button: {
+            href: "https://Cap3ya.github.io/SignupPage/signUpPage.html",
+            text: "Sign up"
+        },
+    },
+    {
+        title: "Library",
+        image: {
+            src: "./images/library.png", 
+            alt: "Screenshot", 
+            height: "205",
+            width: "364",
+        },
+        skills: ["HTML", "CSS", "JavaScript"],
+        text: "Use localStorage and JSON to add, remove and edit books from your library.",
+        button: {
+            href: "https://Cap3ya.github.io/Library/library.html",
+            text: "Store them"
+        },
+    },
+];
--- main.js
+++ main.js
@@ -1,70 +1,7 @@
-/// MY_PROJECTS DATABASE \\\
-const projects = [
-    {
-        title: "Rock Paper Scissors",
-        image: {
-            src: "./images/rockPaperScissors.png", 
-            alt: "Screenshot", 
-            height: "205",
-            width: "364",
-        },
-        skills: ["HTML", "CSS", "JavaScript"],
-        text: "Play against a well behaved bot and try, in five rounds, to win the game!",
-        button: {
-            href: "https://Cap3ya.github.io/RockPaperScissors/rockPaperScissors.html",
-            text: "Beat him"
-        },
-    },
-    {
-        title: "Calculator",
-        image: {
-            src: "./images/calculator.png", 
-            alt: "Screenshot", 
-            height: "205",
-            width: "364",
-        },
-        skills: ["HTML", "CSS", "JavaScript"],
-        text: "A calculator that allows to perform the four basic arithmetic operations.",
-        button: {
-            href: "https://Cap3ya.github.io/Calculator/calculator.html",
-            text: "Sum up"
-        },
-    },
-    {
-        title: "Sign up Page",
-        image: {
-            src: "./images/signupPage.png", 
-            alt: "Screenshot", 
-            height: "205",
-            width: "364",
-        },
-        skills: ["HTML", "CSS", "JavaScript"],
-        text: "A sign up page that uses forms, inputs and sanitization to signup.",
-        button: {
-            href: "https://Cap3ya.github.io/SignupPage/signUpPage.html",
-            text: "Sign up"
-        },
-    },
-    {
-        title: "Library",
-        image: {
-            src: "./images/library.png", 
-            alt: "Screenshot", 
-            height: "205",
-            width: "364",
-        },
-        skills: ["HTML", "CSS", "JavaScript"],
-        text: "Use localStorage and JSON to add, remove and edit books from your library.",
-        button: {
-            href: "https://Cap3ya.github.io/Library/library.html",
-            text: "Store them"
-        },
-    },
-];
+import { projects } from './assets/projects.js'
 /// RENDER SKILLS LIST & NUM OF OCCURENCES TO DOC.MY_SKILLS \\\
 (function Skills() {
-
-// Get skills from projects & Concatenate them
+    // Get skills from projects & Concatenate them
 const skills = new Array
 projects.forEach(project => project.skills.forEach(skill => skills.push(skill)));
 
@@ -75,7 +12,7 @@ for (let skill of skills) {
             skillObj[skill] += 1
 } 
 console.log(skillObj)
-// Append skill + # of occurences to #mySkills
+    // Append skill + # of occurences to #mySkills
 const root = document.getElementById('mySkills')
 for (let skill in skillObj) {
     const p = document.createElement('p')
