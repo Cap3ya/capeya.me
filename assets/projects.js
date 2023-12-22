f9511659e91a6ec762b2aea9544093ddccf351a2
WeAreDwwm2023 <piyiwo9104@cabose.com>
2023-12-22 12:53:02 +0100

    Reduce Afpa's description length

--- assets/projects.js
+++ assets/projects.js
@@ -98,7 +98,7 @@ export const projects = [
             width: "365",
         },
         skills: ["Figma", "HTML", "CSS"],
-        text: "AFPA's project. Had to wireframe, mockup and prototype a 5 pages responsive website.",
+        text: "Had to wireframe, mockup and prototype a 5 pages responsive website.",
         button: {
             href: "https://cap3ya.github.io/Afpa/index.html",
             text: "Check it out"
