8554161d028cad15b3729026aee3ef24efb0f5a9
WeAreDwwm2023 <piyiwo9104@cabose.com>
2024-01-25 18:39:28 +0100

    update urls from githubPage to vercel

--- assets/projects.js
+++ assets/projects.js
@@ -10,7 +10,7 @@ export const projects = [
         skills: ["HTML", "CSS", "TypeScript"],
         text: "Play against a well behaved bot and try to win the game!",
         button: {
-            href: "https://Cap3ya.github.io/rockPaperScissors/index.html",
+            href: "https://rock-paper-scissors-umber-ten.vercel.app/",
             text: "Live preview"
         },
     },
@@ -25,22 +25,7 @@ export const projects = [
         skills: ["HTML", "CSS", "JavaScript"],
         text: "A calculator that allows to perform the four basic arithmetic operations.",
         button: {
-            href: "https://Cap3ya.github.io/Calculator/calculator.html",
-            text: "Live preview"
-        },
-    },
-    {
-        title: "Sign up Page",
-        image: {
-            src: "./images/signupPage.png",
-            alt: "Screenshot",
-            height: "205",
-            width: "364",
-        },
-        skills: ["HTML", "CSS", "JavaScript"],
-        text: "A sign up page that uses forms, inputs and sanitization to signup.",
-        button: {
-            href: "https://Cap3ya.github.io/SignupPage/signUpPage.html",
+            href: "https://calculator-chi-two-62.vercel.app/",
             text: "Live preview"
         },
     },
@@ -55,7 +40,7 @@ export const projects = [
         skills: ["HTML", "CSS", "JavaScript"],
         text: "Use localStorage and JSON to add, remove and edit books from your library.",
         button: {
-            href: "https://Cap3ya.github.io/Library/library.html",
+            href: "https://library-self-three.vercel.app",
             text: "Live preview"
         },
     },
@@ -70,7 +55,7 @@ export const projects = [
         skills: ["HTML", "CSS", "JavaScript", "HTMX"],
         text: "Read the rules, pick your character and play with a friend or againt an AI.",
         button: {
-            href: "https://Cap3ya.github.io/TicTacToe/index.html",
+            href: "https://tic-tac-toe-chi-gold-46.vercel.app/",
             text: "Live preview"
         },
     },
@@ -85,7 +70,7 @@ export const projects = [
         skills: ["HTML", "CSS", "JavaScript", "WebPack"],
         text: "Full javaScript and bundled with WebPack. Use tabs to navigate between pages.",
         button: {
-            href: "https://cap3ya.github.io/Restaurant/dist/index.html",
+            href: "https://restaurant-khaki-omega.vercel.app",
             text: "Live preview"
         },
     },
@@ -100,7 +85,7 @@ export const projects = [
         skills: ["Figma", "HTML", "CSS"],
         text: "Had to wireframe, mockup and prototype a 5 pages responsive website.",
         button: {
-            href: "https://cap3ya.github.io/Afpa/index.html",
+            href: "https://afpa-dwwm.vercel.app/",
             text: "Live preview"
         },
     },
@@ -115,7 +100,7 @@ export const projects = [
         skills: ["HTML", "CSS", "JavaScript"],
         text: "Front end to-do list. Login or signup. CRUD, filter and sort tasks.",
         button: {
-            href: "https://cap3ya.github.io/todolist/index.html",
+            href: "https://todolist-nu-flame.vercel.app/",
             text: "Live preview"
         },
     },
@@ -130,7 +115,7 @@ export const projects = [
         skills: ["HTML", "CSS", "JavaScript", "React"],
         text: "Don't start your resume from scratch. This site uses react's useState.",
         button: {
-            href: "https://resume-builder-zeta-beryl.vercel.app/",
+            href: "https://resume-builder-rho-eight.vercel.app/",
             text: "Live preview"
         },
     },
